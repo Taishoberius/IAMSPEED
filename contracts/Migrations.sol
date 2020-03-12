346fe896917aad99a7cb01d0af5254383ef0c9a2
Florian <florian.mazuy@gmail.com>
2020-03-12 16:03:26 +0100

    wow

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,75 +1,18 @@
+pragma solidity >=0.4.21 <0.7.0;
 
-pragma solidity ^0.6.1;
-import "./DateTime.sol";
-// import "./Feeder.sol";
+contract Migrations {
+  address public owner;
+  uint public last_completed_migration;
 
-contract Trading {
-    uint harryPotterusModulus = 10 ** 16;
-    uint starterWallet = 1000;
+  constructor() public {
+    owner = msg.sender;
+  }
 
-    struct Contest {
-        uint id;
-        /*uint startDate;
-        uint endDate;*/
-    }
+  modifier restricted() {
+    if (msg.sender == owner) _;
+  }
 
-    struct Trader {
-        uint wallet;
-        uint ethers;
-        address trader;
-    }
-
-    Contest[] contests;
-    mapping(uint => Trader[]) internal contestForTraders;
-    
-
-    function createContest() public {
-        Contest memory contest = Contest(uint(keccak256(abi.encodePacked(now, msg.sender))) % harryPotterusModulus);
-        contests.push(contest);
-        joinContest(contest.id);
-    }
-
-    function joinContest(uint _contestId) public {
-        contestForTraders[_contestId].push(Trader(starterWallet, 0, msg.sender));
-    }
-
-    function trade(uint _amount, uint _contestId, bool buying) public {
-        if (buying) {
-            buy(_amount, _contestId);
-            return;
-        }
-    
-        sell(_amount, _contestId);
-    }
-
-    function buy(uint _amount, uint _contestId) private {
-        Trader memory trader = getTraderForContest(_contestId);
-        require((trader.wallet - _amount) >= 0, "No Tune ma gueule");
-        trader.wallet -= _amount;
-        trader.ethers += _amount * getPrice();
-    }
-
-    function sell(uint _amount, uint _contestId) private {
-        Trader memory trader = getTraderForContest(_contestId);
-        trader.wallet += _amount;
-        trader.ethers -= _amount * getPrice();
-    }
-
-    function getTraderForContest(uint _contestId) private returns (Trader memory) {
-        Trader[] memory traders = contestForTraders[_contestId];
-        for (uint index=0; index < traders.length; index++) {
-            if (traders[index].trader == msg.sender) {
-                return traders[index];
-            }
-        }
-
-        return Trader(1000, 1, msg.sender);
-    }
-
-    function getPrice() private returns (uint) {
-        return 2;
-    }
-    
-    function checkContestStatus(uint _contestId) public {
-    }
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
 }
--- contracts/Trading.sol
+++ contracts/Trading.sol
@@ -1,6 +1,4 @@
-
-pragma solidity ^0.6.1;
-import "./DateTime.sol";
+pragma solidity >=0.4.21 <0.7.0;
 // import "./Feeder.sol";
 
 contract Trading {
